92e9f538e53aa471fc5d488c7491fc22e2d82893
VelkaB <133142401+VelkaB@users.noreply.github.com>
2023-06-08 12:54:17 +0300

    Add files via upload

---
+++ A3A/addons/patcom/CfgFunctions.hpp
@@ -0,0 +1,37 @@
+class CfgFunctions {
+    class A3A {
+        class Artillery {
+            file = QPATHTOFOLDER(functions\Artillery);
+            class artilleryAdd {};
+            class artilleryDangerClose {};
+            class artilleryFireMission {};
+            class getReloadTime {};
+        };
+
+        class Patcom {
+            file = QPATHTOFOLDER(functions\Patcom);
+            class clearVehicleCargo {};
+            class debugText3D {};
+            class debug3DPath {};
+            class getSafePos {};
+            class patrolArea {};
+            class patrolArmStatics {};
+            class patrolAttack {};
+            class patrolBuildingEnterable {};
+            class patrolCivilianCommander {};
+            class patrolCommander {};
+            class patrolCreateWaypoint {};
+            class patrolDefend {};
+            class patrolEnterableBuildings {};
+            class patrolGetEnemies {};
+            class patrolGroupGarrison {};
+            class patrolGroupVariables {};
+            class patrolHandleFormation {};
+            class patrolInit {};
+            class patrolLoop {};
+            class patrolSearchBuilding {};
+            class patrolSetCombatModes {};
+            class patrolWater {};
+        };
+    };
+};
